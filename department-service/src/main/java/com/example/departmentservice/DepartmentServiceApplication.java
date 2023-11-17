354326381c051f8271f7fb469a3dbbfdbf508dac
Cary Guca <cary.guca@gmail.com>
2023-11-16 20:12:09 -0600

    Added organization service

--- department-service/src/main/java/com/example/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/example/departmentservice/DepartmentServiceApplication.java
@@ -1,8 +1,33 @@
 package com.example.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service",
+				description = "Department Service REST API Document",
+				version = "1.0",
+				contact = @Contact(
+						name = "Cary",
+						email = "com.example",
+						url = "com.example"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "com.example"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department Service doc",
+				url = "com.example"
+		)
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
--- department-service/src/main/java/com/example/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/example/departmentservice/controller/DepartmentController.java
@@ -2,11 +2,18 @@ package com.example.departmentservice.controller;
 
 import com.example.departmentservice.dto.DepartmentDto;
 import com.example.departmentservice.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Department Controller",
+        description = "Department Controller Exposes REST APIs for Department Service"
+)
 @RestController
 @RequestMapping("api/departments")
 @AllArgsConstructor
@@ -14,6 +21,14 @@ public class DepartmentController
 {
     private DepartmentService departmentService;
 
+    @Operation(
+            summary = "Get department REST API",
+            description = "Used to get department from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "200 status OK"
+    )
     @GetMapping("{department-code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String code)
     {
@@ -21,6 +36,14 @@ public class DepartmentController
         return new ResponseEntity<>(departmentDto, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Save department REST API",
+            description = "Used to save department in the database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "201 status created"
+    )
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto)
     {
